35e0b479579a6101063b7228c759fdbd5db6e450
gpabois <gael.pabois@gmail.com>
2023-03-07 13:29:07 +0100

    up

--- cagouille/settings.py
+++ cagouille/settings.py
@@ -29,7 +29,8 @@ SECRET_KEY = env.str('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool('DEBUG', default=False)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = env.list("ALLOWED_HOSTS", default=[])
+CSRF_TRUSTED_ORIGINS = env.list("CSRF_TRUSTED_ORIGINS", default=[])
 
 # Application definition
 GEORISQUE_API_URL = env.str('GEORISQUE_API_URL', default='https://www.georisques.gouv.fr/api/v1')
